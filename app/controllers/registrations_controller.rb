ee2b177f73baf09b4f368cde2de2e848e9fbb3db
Bob <rjwhitmercc@gmail.com>
2026-02-26 13:23:02 -0700

    remove dead code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,2 @@
 class RegistrationsController < ApplicationController
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      send_email_verification
-      render json: @user, status: :created
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
-
-  private
-    def user_params
-      params.permit(:email, :password, :password_confirmation)
-    end
-
-    def send_email_verification
-      UserMailer.with(user: @user).email_verification.deliver_later
-    end
 end
